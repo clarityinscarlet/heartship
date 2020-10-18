985c96c1b1b48f588d682341c278c2b4eda15eab
claire <clarityinscarlet@gmail.com>
2020-10-18 18:17:15 -0400

    updates

--- heartship.js
+++ heartship.js
@@ -6,20 +6,33 @@ window.onload = () => {
 function changeColor(ship) {
     switch (ship) {
         case 'bingqiu':
-            document.body.style.background = '#dbfff2';
+            document.body.style.background = '#dbffeb';
             document.body.style.color = '#872427';
+            changeButtonColor('#dbffeb', '#872427');
             break;
         case 'wangxian':
             document.body.style.background = '#d7f2ff';
             document.body.style.color = '#8f0e16';
+            changeButtonColor('#d7f2ff', '#8f0e16');
             break;
         case 'hualian':
             document.body.style.background = '#fff3db';
             document.body.style.color = '#9f1018';
+            changeButtonColor('#fff3db', '#9f1018');
             break;
         default:
             document.body.style.background = '#0c1524';
             document.body.style.color = '#e3e3e3';
+            changeButtonColor('#0c1524', '#e3e3e3');
             break;
+    }
+}
+
+function changeButtonColor(background, color) {
+    var elements = document.getElementsByClassName("ship");
+    for(var i=0; i<elements.length; i++) {
+        elements[i].style.background = background;
+        elements[i].style.borderColor = color;
+        elements[i].style.color = color;
     }
 }
